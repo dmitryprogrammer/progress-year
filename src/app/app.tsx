e4fe7c2bd8db659ad6731a75465dd58e80f23d81
dmitryprogrammer <dpischalka@gmail.com>
2025-01-16 10:53:08 +0300

    feat(basics): created head component and reorginized app component.

--- src/app/app.tsx
+++
@@ -1,7 +0,0 @@
-export function App() {
-  return (
-    <div>
-      <h1>Progress year</h1>
-    </div>
-  );
-}
---
+++ src/components/head/head.tsx
@@ -0,0 +1,5 @@
+export const Head = () => (
+  <>
+    <h1>Progress year</h1>
+  </>
+);

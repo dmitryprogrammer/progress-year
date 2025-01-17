7ef057100e0c85d85e1ce32a03f6cfa029e83f6c
dmitryprogrammer <dpischalka@gmail.com>
2025-01-17 13:17:05 +0300

    feat(basics): installed styled-components.

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-decoration: underline;
+`;
+
+const HeadWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 export const Head = () => (
-  <>
-    <h1>Progress year</h1>
-  </>
+  <HeadWrapper>
+    <Title>Progress year</Title>
+  </HeadWrapper>
 );

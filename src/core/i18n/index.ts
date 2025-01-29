306a3d2c6ecb4a98f37ce56f9e1398a09249aac2
dmitryprogrammer <dpischalka@gmail.com>
2025-01-29 23:44:21 +0300

    feat(basics): prepared i18n core.

---
+++ src/core/i18n/index.ts
@@ -0,0 +1,19 @@
+import i18n from "i18next";
+import I18NextHttpBackend from "i18next-http-backend";
+import {initReactI18next} from "react-i18next";
+
+export enum LANGUAGES {
+  RU = "ru",
+  EN = "en",
+}
+
+export const SUPPORTED_LANGUAGES = [LANGUAGES.RU, LANGUAGES.EN];
+
+i18n
+  .use(I18NextHttpBackend)
+  .use(initReactI18next)
+  .init({
+    lng: LANGUAGES.RU,
+    supportedLngs: SUPPORTED_LANGUAGES,
+    interpolation: {escapeValue: false},
+  });

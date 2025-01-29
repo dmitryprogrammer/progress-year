14ceec3ea61b4ae1debad59b4c6681d70c59cff5
dmitryprogrammer <dpischalka@gmail.com>
2025-01-29 23:44:44 +0300

    feat(basics): imported i18n to project.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {App} from "./components/app/app";
 import "./assets/styles/index.scss";
+import "./core/i18n";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>

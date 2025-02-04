611fcd0dcfc6ae123473a2f571a49df8abd165b4
dmitryprogrammer <dpischalka@gmail.com>
2025-02-04 12:41:22 +0300

    feat(basics): set default background + fixes.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
+import {Flex} from "antd";
 import {LangSwitchBtns} from "../../core/i18n/components/lang-switch-btns/lang-switch-btns";
 import {Head} from "../head/head";
 
 export function App() {
   return (
-    <>
+    <Flex gap="middle" wrap>
       <LangSwitchBtns />
       <Head />
-    </>
+    </Flex>
   );
 }

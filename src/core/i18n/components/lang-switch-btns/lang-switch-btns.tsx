28c93feffba5cdc4511cbe19209ae56b52d9979a
dmitryprogrammer <dpischalka@gmail.com>
2025-02-04 12:26:07 +0300

    feat(basics): created lang switch component.

---
+++ src/core/i18n/components/lang-switch-btns/lang-switch-btns.tsx
@@ -0,0 +1,33 @@
+import {useTranslation} from "react-i18next";
+import {LANGUAGES} from "../..";
+import {useState} from "react";
+import {Radio} from "antd";
+
+export function LangSwitchBtns() {
+  const [languagesButtonNames] = useState(
+    Object.values(LANGUAGES).map((lang: LANGUAGES) => ({
+      label: lang,
+      value: lang,
+    })),
+  );
+  const [currentLanguage, setCurrentLanguage] = useState(LANGUAGES.RU);
+
+  const {i18n} = useTranslation();
+
+  const switchLanguage = (language: LANGUAGES): void => {
+    i18n.changeLanguage(language);
+    setCurrentLanguage(language);
+  };
+
+  return (
+    <>
+      <Radio.Group
+        value={currentLanguage}
+        onChange={(event) => switchLanguage(event.target.value)}
+        options={languagesButtonNames}
+        optionType="button"
+        buttonStyle="outline"
+      ></Radio.Group>
+    </>
+  );
+}

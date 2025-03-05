27862374198d2b387c8d061b3c31796fccbfad15
dmitryprogrammer <dpischalka@gmail.com>
2025-03-05 11:48:17 +0300

    feat(basics): git rid from styled css and recomposed header content.

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,22 +1,9 @@
-import {useTranslation} from "react-i18next";
-import {styled} from "styled-components";
-
-const Title = styled.h1`
-  text-decoration: underline;
-`;
-
-const HeadWrapper = styled.div`
-  width: 100%;
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-`;
+import {LangSwitchBtns} from "@core/i18n/components/lang-switch-btns/lang-switch-btns";
 
 export const Head = () => {
-  const {t} = useTranslation();
   return (
-    <HeadWrapper>
-      <Title>{t("title")}</Title>
-    </HeadWrapper>
+    <header className="header">
+      <LangSwitchBtns />
+    </header>
   );
 };

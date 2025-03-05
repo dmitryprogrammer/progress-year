ee5485c16879838123c19ab7592c69c1d8a8a192
dmitryprogrammer <dpischalka@gmail.com>
2025-03-05 11:49:07 +0300

    feat(basics): got rid from flex container from app components.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,10 @@
-import {Flex} from "antd";
-
+import {ProgressYearContainer} from "@containers/progress-year/progress-year-container/progress-year-container";
 import {Head} from "../head/head";
-import {LangSwitchBtns} from "@core/i18n/components/lang-switch-btns/lang-switch-btns";
 export function App() {
   return (
-    <Flex gap="middle" wrap>
-      <LangSwitchBtns />
+    <main>
       <Head />
-    </Flex>
+      <ProgressYearContainer />
+    </main>
   );
 }

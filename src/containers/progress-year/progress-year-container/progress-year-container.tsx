99e930603f45fa232b420c7c32eba20c1153c59b
dmitryprogrammer <dpischalka@gmail.com>
2025-03-14 12:34:07 +0300

    feat(progress-year-functional): improved logic for progress-year-container component.

--- src/containers/progress-year/progress-year-container/progress-year-container.tsx
+++ src/containers/progress-year/progress-year-container/progress-year-container.tsx
@@ -1,9 +1,17 @@
+import {useEffect, useState} from "react";
 import {ProgressYearTitle} from "../progress-year-title/progress-year-title";
+import {getDaysGoneFromYearInPercents} from "@core/utils";
+import {ProgressYearBar} from "../progress-year-bar/progress-year-bar";
 
 export function ProgressYearContainer() {
+  const [yearGonePercents, setYearGonePercents] = useState<number>(0);
+  useEffect(() => {
+    setYearGonePercents(getDaysGoneFromYearInPercents());
+  }, [yearGonePercents]);
   return (
     <div className="progress-year-container">
-      <ProgressYearTitle />
+      <ProgressYearTitle yearGonePercents={yearGonePercents} />
+      <ProgressYearBar yearGonePercents={yearGonePercents} />
     </div>
   );
 }

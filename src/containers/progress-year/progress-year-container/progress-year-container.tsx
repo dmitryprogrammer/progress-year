aeef068812d8dfe12b1084cd5eda8e0f7970816e
dmitryprogrammer <dpischalka@gmail.com>
2025-03-14 11:53:15 +0300

    feat(progress-year-functional): basic styles for progress-year-container.

--- src/containers/progress-year/progress-year-container/progress-year-container.tsx
+++ src/containers/progress-year/progress-year-container/progress-year-container.tsx
@@ -2,7 +2,7 @@ import {ProgressYearTitle} from "../progress-year-title/progress-year-title";
 
 export function ProgressYearContainer() {
   return (
-    <div>
+    <div className="progress-year-container">
       <ProgressYearTitle />
     </div>
   );

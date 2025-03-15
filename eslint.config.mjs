36b685a2efd3fc99f38b97fe059bdef0160effec
dmitryprogrammer <dpischalka@gmail.com>
2025-03-15 10:45:49 +0300

    feat(environment-preparation): added eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,6 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    ignores: ["dist/*", "jest.config.js"],
+    ignores: ["dist/*", "jest.config.js", "server/*"],
   },
 );

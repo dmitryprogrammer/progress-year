ff2e7820517fe25dc4749ff90d8a46b7d968332c
dmitryprogrammer <dpischalka@gmail.com>
2025-03-18 21:15:05 +0300

    feat(set-default-language): resolved package differnece between react and antd design.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
+import "@ant-design/v5-patch-for-react-19";
 import {App} from "./components/app/app";
 import "./assets/styles/index.scss";
 import "./core/i18n";
